9fbd2f1837d8c94bbe64ed6654cdeea02103cbd2
Felipe Mateus <contato@felipefm32.com.br>
2020-09-21 10:39:56 -0300

    Update

--- mario_song.c
+++ mario_song.c
@@ -19,168 +19,20 @@ Modified by Michael Howard for the Super Mario Bros. theme Arduino sketch
 #include <unistd.h>
 #define sleep(x) usleep(x)
 #endif
-#include "pitches.h" //Header file with pitch definitions
+
+#include "lib/notes.h"
+#include "lib/times.h"
 
 
-//Note Array
-int notes_start[] = {
- NOTE_E5, NOTE_E5, 0, NOTE_E5, 0, NOTE_C5, NOTE_E5, 0, 
- NOTE_G5, 0, NOTE_G4, 0
-};
- 
-int notes_tune1[] = {
- NOTE_C5, 0, NOTE_G4, 0, NOTE_E4, 0, NOTE_A4, 0, 
- NOTE_B4, 0, NOTE_AS4, NOTE_A4, 0, NOTE_G4, NOTE_E4, NOTE_G5,
- 
- NOTE_A5, 0, NOTE_F5, NOTE_G5, 0, NOTE_E5, 0, NOTE_C5,
- NOTE_D5, NOTE_B4, 0,
-};
- 
-int notes_tune2[] = {
-  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 
-  0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
- 
-  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5,
-  0, NOTE_C6, 0, NOTE_C6, NOTE_C6, 0,
-
- 
- 0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 
- 0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
- 
- 0, NOTE_DS5, 0, NOTE_D5, 0,
- NOTE_C5, 0,
-};
- 
-int bridge[] = {
-  NOTE_C5, NOTE_C5, 0, NOTE_C5, 0, NOTE_C5, NOTE_D5, 0, 
-  NOTE_E5, NOTE_C5, 0, NOTE_A4, NOTE_G4, 0,
- 
-  NOTE_C5, NOTE_C5, 0, NOTE_C5, 0, NOTE_C5, NOTE_D5, NOTE_E5,
-  0,
- 
-  NOTE_C5, NOTE_C5, 0, NOTE_C5, 0, NOTE_C5, NOTE_D5, 0,
-  NOTE_E5, NOTE_C5, 0, NOTE_A4, NOTE_G4, 0,
- 
-  NOTE_E5, NOTE_E5, 0, NOTE_E5, 0, NOTE_C5, NOTE_E5, 0,
-  NOTE_G5, 0, NOTE_G4, 0,
-};
- 
- int switchup[] = {
- NOTE_E5, NOTE_C5, 0, NOTE_G4, 0, NOTE_GS4, 0, 
- NOTE_A4, NOTE_F5, 0, NOTE_F5, NOTE_A4, 0,
- 
- NOTE_B4, NOTE_A5, NOTE_A5, NOTE_A5, NOTE_G5, NOTE_F5,
- NOTE_E5, NOTE_C5, 0, NOTE_A4, NOTE_G4, 0,
-
- 
- NOTE_E5, NOTE_C5, 0, NOTE_G4, 0, NOTE_GS4, 0,
- NOTE_A4, NOTE_F5, 0, NOTE_F5, NOTE_A4, 0,
- 
- NOTE_B4, NOTE_F5, 0, NOTE_F5, NOTE_F5, NOTE_E5, NOTE_D5,
- NOTE_C5, NOTE_E4, 0, NOTE_E4, NOTE_C4, 0
-};
- 
- int fanfare[] = {
- NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3, NOTE_C4, NOTE_CS4, NOTE_D4, NOTE_DS4, NOTE_E4, NOTE_F4, NOTE_FS4,
- NOTE_G4, NOTE_GS4, NOTE_A4, NOTE_AS4, NOTE_B4, NOTE_C5, NOTE_CS5, NOTE_D5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_FS5,
- NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_AS5, NOTE_B5, NOTE_C6, NOTE_CS6, NOTE_D6, NOTE_DS6, NOTE_E6, 0, 
- 
- NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, NOTE_E5,
- NOTE_G5, NOTE_E5,
- 
- NOTE_GS3, NOTE_C4, NOTE_DS4, NOTE_GS4, NOTE_C5, NOTE_DS5,
- NOTE_GS5, NOTE_DS5,
- 
- NOTE_AS3, NOTE_D4, NOTE_F4, NOTE_AS4, NOTE_D5, NOTE_F5,
- NOTE_AS5, NOTE_B5, NOTE_B5, NOTE_B5, 
- NOTE_C6
-};
-
-//The Duration of each note (in ms)
-int times_start[] = {
- 150, 150, 150, 150, 150, 150, 150, 150,
- 150, 450, 150, 450
-};
- 
-int times_tune1[] = {
- 150, 300, 150, 300, 150, 300, 150, 150, 
- 150, 150, 150, 150, 150, 200, 200, 200,
- 
- 150, 150, 150, 150, 150, 150, 150, 150,
- 150, 150, 300,
-};
- 
-int times_tune2[] = {
- 300, 150, 150, 150, 150, 150, 150,
- 150, 150, 150, 150, 150, 150, 150, 150,
- 
- 300, 150, 150, 150, 150, 150, 150,
- 150, 150, 150, 150, 150, 450,
-
- 300, 150, 150, 150, 150, 150, 150,
- 150, 150, 150, 150, 150, 150, 150, 150,
- 
- 300, 150, 300, 150, 300,
- 150, 1050
-};
- 
-int times_bridge[] = {
- 150, 150, 150, 150, 150, 150, 150, 150, 
- 150, 150, 150, 150, 150, 450,
- 
- 150, 150, 150, 150, 150, 150, 150, 150,
- 1200,
-
- 
- 150, 150, 150, 150, 150, 150, 150, 150, 
- 150, 150, 150, 150, 150, 450,
- 
- 150, 150, 150, 150, 150, 150, 150, 150, 
- 150, 450, 150, 450
-};
- 
-int times_switch_up[] = {
- 150, 150, 150, 150, 300, 150, 150,
- 150, 150, 150, 150, 150, 450,
- 
- 200, 200, 200, 200, 200, 200,
- 150, 150, 150, 150, 150, 450,
- 
- 150, 150, 150, 150, 300, 150, 150,
- 150, 150, 150, 150, 150, 450,
- 
- 150, 150, 150, 150, 200, 200, 200,
- 150, 150, 150, 150, 150, 450
-};
- 
-int times_fanfare[] = {
- 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 
- 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 
- 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 946,
- 
- 167, 167, 167, 167, 167, 167,
- 500, 500,
- 
- 167, 167, 167, 167, 167, 167,
- 500, 500,
- 
- 167, 167, 167, 167, 167, 167,
- 500, 167, 167, 167,
- 1000,
-};
-
-
-void sheet_music(int notes[], int times[], size_t arr_size)
-{
+void sheet_music(int notes[], int times[], size_t arr_size){
     int i;
-	for ( i = 0; i < arr_size; i++) //start loop
-	{
+	for ( i = 0; i < arr_size; i++){
 		Beep(notes[i], times[i]);
 		sleep(times[i]);
 	}
 }
 
-void main(){
+int main(){
 	sheet_music(notes_start, times_start, sizeof(notes_start) / sizeof(notes_start[0]));
 	sheet_music(notes_tune1, times_tune1, sizeof(notes_tune1) / sizeof(notes_tune1[0]));
 	sheet_music(notes_tune1, times_tune1, sizeof(notes_tune1) / sizeof(notes_tune1[0]));
@@ -194,4 +46,5 @@ void main(){
 	sheet_music(bridge, times_bridge, sizeof(bridge) / sizeof(times_bridge[0]));
 	sheet_music(switchup, times_switch_up, sizeof(switchup) / sizeof(switchup[0]));
 	sheet_music(fanfare, times_fanfare, sizeof(fanfare) / sizeof(times_fanfare[0]));
+	return 0;
 }
