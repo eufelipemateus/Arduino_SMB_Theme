e7e456d73e99aa63a95d71e8291e66587d170014
Felipe Mateus <contato@felipefm32.com.br>
2020-09-20 21:23:38 -0300

    Convert ino to c.
    Converting arduino code to C.

--- mario_song.c
+++ mario_song.c
@@ -1,21 +1,26 @@
 /*
 Exploring Arduino - Code Listing 5-1: Arduino Music Player
 http://www.exploringarduino.com/content/ch5
-
 Copyright 2013 Jeremy Blum ( http://www.jeremyblum.com )
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License v3 as published by
 the Free Software Foundation.
-
 Modified by Michael Howard for the Super Mario Bros. theme Arduino sketch
-
 */
 
 //Plays a song on a speaker
-
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#ifdef _WIN32
+#include <Windows.h>
+#define sleep(x) Sleep(x)
+#else
+#include <unistd.h>
+#define sleep(x) usleep(x)
+#endif
 #include "pitches.h" //Header file with pitch definitions
 
-const int SPEAKER=9;  //Speaker Pin
 
 //Note Array
 int notes_start[] = {
@@ -165,10 +170,17 @@ int times_fanfare[] = {
 };
 
 
-
-void setup()
+void sheet_music(int notes[], int times[], size_t arr_size)
 {
-	//Play each note for the right duration
+    int i;
+	for ( i = 0; i < arr_size; i++) //start loop
+	{
+		Beep(notes[i], times[i]);
+		sleep(times[i]);
+	}
+}
+
+void main(){
 	sheet_music(notes_start, times_start, sizeof(notes_start) / sizeof(notes_start[0]));
 	sheet_music(notes_tune1, times_tune1, sizeof(notes_tune1) / sizeof(notes_tune1[0]));
 	sheet_music(notes_tune1, times_tune1, sizeof(notes_tune1) / sizeof(notes_tune1[0]));
@@ -182,18 +194,4 @@ void setup()
 	sheet_music(bridge, times_bridge, sizeof(bridge) / sizeof(times_bridge[0]));
 	sheet_music(switchup, times_switch_up, sizeof(switchup) / sizeof(switchup[0]));
 	sheet_music(fanfare, times_fanfare, sizeof(fanfare) / sizeof(times_fanfare[0]));
-}
-
-void loop()
-{
-  //Press the reset button to play again.
-}
-
-void sheet_music(int notes[], int times[], size_t arr_size)
-{
-	for (int i = 0; i < arr_size; i++) //start loop
-	{
-		tone(SPEAKER, notes[i], times[i]);
-		delay(times[i]);
-	}
 }
